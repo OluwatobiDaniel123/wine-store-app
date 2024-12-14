53b6acd0cb18791a05c0da38fbc523bd774e0853
OLUWATOBI DANIEL <danieloluwatobi765@gmail.com>
2024-12-14 17:33:05 +0100

    Added another routes again..

--- client/src/pages/Account/Login.jsx
+++ client/src/pages/Account/Login.jsx
@@ -38,30 +38,27 @@ const Login = () => {
     //   newLogin
     // );
 
-    // fetch(
-    //   "https://wine-store-app-server.vercel.app/?vercelToolbarCode=XHNTyrPkmSbLr1z/api/auth/login",
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify(newLogin),
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //   }
-    // )
-    //   .then((response) => response.json())
-    //   .then((data) => console.log(data))
-    //   .catch((error) => console.error(error));
+    fetch("https://wine-store-app-backend.vercel.app/api/auth/login", {
+      method: "POST",
+      body: JSON.stringify(newLogin),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
 
-    try {
-      const { data } = await axios.post(
-        "https://wine-store-app-backend.vercel.app/api/auth/login",
-        newLogin
-      );
+    // try {
+    //   const { data } = await axios.post(
+    //     "https://wine-store-app-backend.vercel.app/api/auth/login",
+    //     newLogin
+    //   );
 
-      console.log("Login Data>>>>>", data);
-    } catch (error) {
-      console.log(error.message);
-    }
+    //   console.log("Login Data>>>>>", data);
+    // } catch (error) {
+    //   console.log(error.message);
+    // }
   };
 
   return (
