6b7c7e64b24c5d31e88bdad9b675a8357c8f98d7
OLUWATOBI DANIEL <danieloluwatobi765@gmail.com>
2024-12-14 13:54:23 +0100

    add new

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const __dirname = path.dirname(__filename);
 // app.use(cors());
 app.use(
   cors({
-    origin: "https://wine-store-app-client.vercel.app/",
+    origin: "*",
     credentials: true,
   })
 );
